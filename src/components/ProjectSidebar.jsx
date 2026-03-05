91afb49e8e264062c1e73a9096e44d4cda2e681a
stwhello <sherinpaul2001@gmail.com>
2026-03-05 11:36:04 +0530

    added sidebar

---
+++ src/components/ProjectSidebar.jsx
@@ -0,0 +1,9 @@
+export default function ProjectSidebar() {
+    return <aside>
+        <h2>Your Projects</h2>
+        <div>
+            <button>+ Add Project</button>
+        </div>
+        <ul></ul>
+    </aside>
+}
