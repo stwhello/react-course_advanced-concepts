26186285361193e049106a8c9fd7cd8cec62f7e5
stwhello <sherinpaul2001@gmail.com>
2026-03-11 16:13:17 +0530

    no project selected tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
+
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSidebar from "./components/ProjectSidebar";
 
 function App() {
+
+  const [] = useState({})
+
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectSidebar />
