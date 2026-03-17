081a88502f2c52bd73529beb0317a74031a3b75a
stwhello <sherinpaul2001@gmail.com>
2026-03-17 13:32:26 +0530

    day 4

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,4 @@
-export default function Input({ label, textarea, ...props }) {
+export default function Input({ label, textarea, ...props }, ref) {
  const classes = 'w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600'
 
   return (
@@ -7,11 +7,11 @@ export default function Input({ label, textarea, ...props }) {
         {label}
       </label>
       {textarea ? (
-        <textarea className={classes}
+        <textarea ref={ref} className={classes}
           {...props}
         />
       ) : (
-        <input className={classes} {...props} />
+        <input ref={ref} className={classes} {...props} />
       )}
     </p>
   );
